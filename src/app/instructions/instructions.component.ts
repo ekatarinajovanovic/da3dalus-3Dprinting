659e957ae00c3587ab63f8d808c10d3961b7234f
ekatarinajovanovic <ekatarina.jovanovic@adesso.de>
2022-07-28 16:47:42 +0200

    com

--- src/app/instructions/instructions.component.ts
+++ src/app/instructions/instructions.component.ts
@@ -9,15 +9,27 @@ import { InstructionsService } from '../service/instructions.service';
 export class InstructionsComponent implements OnInit {
   public stepsList: any = [];
   public currentStep: number = 0;
+  public list_size: number = 0;
+  public current_instruction: string ="";
+
+ 
+  
   constructor(private InstructionsService: InstructionsService) { }
 
 
   ngOnInit(): void {
     this.getAllSteps();
+
+    this.current_instruction= this.stepsList[this.currentStep].instruction_text;
+   
+    
+
   }
   getAllSteps() {
     this.InstructionsService.getStepsJson().subscribe(res => {
       this.stepsList = res.steps;
-    })
+    });
+    
   }
+  
 }
