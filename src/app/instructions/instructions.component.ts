50d7bb85d5cbd420aa17d5201ea8cdf5a366ed7a
ekatarinajovanovic <ekatarina.jovanovic@adesso.de>
2022-08-01 13:59:03 +0200

    c

--- src/app/instructions/instructions.component.ts
+++ src/app/instructions/instructions.component.ts
@@ -11,6 +11,7 @@ import { ActivatedRoute, Router} from '@angular/router';
 export class InstructionsComponent implements OnInit {
   public stepsList: any = [];
   public currentStep : number = 0;
+  public currStep: number=0;
   public list_size: number = 0;
   public current_instruction: string ="";
   public problemsdiv: boolean = false;
@@ -24,7 +25,8 @@ export class InstructionsComponent implements OnInit {
   ngOnInit(): void {
     this.rout.queryParams.subscribe((queryParams) => {
       this.currentStep=queryParams['id'];});
-   
+      this.rout.params.subscribe((params) => {
+        this.currStep=params['id'];});
    this.getAllSteps();
    
         
@@ -44,12 +46,10 @@ export class InstructionsComponent implements OnInit {
   }
 
   nextInstruction(){
-    this.currentStep++;
+    this.currStep++;
     this.router.navigate(
-      ['/instructions'],
-      {
-        queryParams: { id: this.currentStep }
-      }
+      ['/instructions', this.currStep]
+     
     )
     
   }
